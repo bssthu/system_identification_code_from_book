29a19be403f51da47af2b4ce867b5295acf3bdea
bssthu <bssthu@users.noreply.github.com>
2015-06-12 11:35:40 +0800

    add inline comment for chap 5

--- RLS.m
+++ RLS.m
@@ -5,6 +5,7 @@
 % 
 
 for k = nMax + 1 : L + nMax
+    % 构造数据向量
     for i = 1:na
         h(i, k) = -z(k - i);
     end
@@ -13,11 +14,13 @@ for k = nMax + 1 : L + nMax
         h(na + i, k) = u(k - i);
     end
     
+    % 辨识算法
     s(k) = h(:, k)' * P(:, :, k-1) * h(:, k) + 1.0;
     Inn(k) = z(k) - h(:, k)' * Theta(:, k-1);
     K(:, k) = P(:, :, k-1) * h(:, k) / s(k);
     P(:, :, k) = P(:, :, k-1) - K(:, k) * K(:, k)' * s(k);
     Theta(:, k) = Theta(:, k-1) + K(:, k) * Inn(k);
     
+    % 损失函数
     J(k) = J(k-1) + Inn(k)^2 / s(k);
 end
